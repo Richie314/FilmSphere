45c27ce51cd21c673cf20540dd43d1e7f9c73b9d
Richie314 <richieinleghorn@gmail.com>
2023-09-10 16:18:20 +0200

    Migliorie

--- Popolamento/ip-ranges.py
+++ Popolamento/ip-ranges.py
@@ -1,5 +1,6 @@
 def generate():
-    with open('asn-country-ipv4.csv', 'r') as file_in, open('ip-ranges.sql', 'w') as file_out:
+    """
+    with open('asn-country-ipv4-num.csv', 'r') as file_in, open('ip-ranges.sql', 'w') as file_out:
         for line_in in file_in:
             # Ip Start, Ip End, Country code
             ip_start, ip_end, country_code = line_in.split(',')
@@ -8,8 +9,30 @@ def generate():
             country_code = country_code.strip()
             assert len(country_code) == 2
             country_code = country_code.upper()
-            line = f'CALL `IpRangeProvaInserireAdesso`(Ip2Int(\'{ip_start}\'), Ip2Int(\'{ip_end}\'), \'{country_code}\');\n'
+            line = f'CALL `IpRangeInserisciAdessoFidato`({ip_start}, {ip_end}, \'{country_code}\', FALSE);\n'
             file_out.write(line)
+    """
+    with open('asn-country-ipv4-num.csv', 'r') as file_in, open('ip-ranges.sql', 'w') as file_out:
+        sql = 'REPLACE INTO `IPRange` (`Paese`, `Inizio`, `Fine`) VALUES\n'
+        old_line = ''
+        file_out.write(sql)
+        for line_in in file_in:
+            # Ip Start, Ip End, Country code
+            ip_start, ip_end, country_code = line_in.split(',')
+            ip_start = ip_start.strip()
+            ip_end = ip_end.strip()
+            country_code = country_code.strip()
+            assert len(country_code) == 2
+            country_code = country_code.upper()
+            
+            this_line = f'(\'{country_code}\', {ip_start}, {ip_end})'
+            if len(old_line) == 0:
+                # First time we write, no comma
+                old_line = '\n\t' + this_line
+            else:
+                old_line = ',\n\t' + this_line
+            file_out.write(old_line)
+        file_out.write(';\n')
                 
 
 if __name__ == '__main__':
